f84a4f876c13b25a436cc8e0a00b8d798e691bd0
mikrob <promikrob@gmail.com>
2022-11-23 22:34:34 +0200

    Start id unit page

--- src/Cards/Unit.js
+++ src/Cards/Unit.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import { useParams } from "react-router";
+import Footer from "./Footer";
 
 export default function Unit() {
   const { id } = useParams();
   console.log(id);
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
   );
 }
