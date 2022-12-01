1194e928ebcee7fd24ee3aca55fbc9f62fa8f871
mikrob <promikrob@gmail.com>
2022-12-01 17:54:57 +0200

    Done Login Page+regex inputs

--- src/CardsJs/Login.js
+++ src/CardsJs/Login.js
@@ -2,8 +2,64 @@ import React from "react";
 import Header from "./CardsHtml/Header";
 import Footer from "./CardsHtml/Footer";
 import "../CardsScc/Login.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState("Email cannot be empty");
+  const [passwordError, setPasswordError] = useState(
+    "Password cannot be empty"
+  );
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, passwordError]);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Incorrect email");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 3 || e.target.value.length > 8) {
+      setPasswordError(
+        "Password must be more than 3 characters and less than 8"
+      );
+      if (!e.target.value) {
+        setPasswordError("Password cannot be empty");
+      }
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setEmailDirty(true);
+        break;
+      case "password":
+        setPasswordDirty(true);
+        break;
+    }
+  };
+
   return (
     <>
       <div className="ShopHeader">
@@ -11,7 +67,69 @@ export default function Login() {
       </div>
 
       <section>
-        <div></div>
+        <h3 className="loginTitle">My Account</h3>
+        <div className="MainBlocks">
+          <div>
+            <div className="loginMainBlock">
+              <div className="login">LOGIN</div>
+              <div className="loginText">
+                If you have an account with us, please log in.
+              </div>
+              <div>
+                {emailDirty && emailError && (
+                  <div style={{ color: "red", fontSize: "13px" }}>
+                    {emailError}
+                  </div>
+                )}
+                <input
+                  onChange={(e) => emailHandler(e)}
+                  value={email}
+                  onBlur={(e) => blurHandler(e)}
+                  name="email"
+                  className="loginInp"
+                  type="email"
+                  placeholder="Email"
+                />
+              </div>
+              <div>
+                {passwordDirty && passwordError && (
+                  <div style={{ color: "red", fontSize: "13px" }}>
+                    {passwordError}
+                  </div>
+                )}
+                <input
+                  onChange={(e) => passwordHandler(e)}
+                  value={password}
+                  onBlur={(e) => blurHandler(e)}
+                  name="password"
+                  className="loginInp"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="loginBtn">
+                <button disabled={!formValid}>SIGN IN</button>
+              </div>
+              <div className="forgotPass">
+                <span className="forgotBtn">Forgot your password?</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="regMainBlock">
+              <div className="regTitle">NEW CUSTOMER?</div>
+              <div className="regText">
+                Registering for this site allows you to access your order status
+                and history. We’ll get a new account set up for you in no time.
+                For this will only ask you for information necessary to make the
+                purchase process faster and easier
+              </div>
+              <Link to="/registration" className="loginLink">
+                <div className="regBtn">CREATE AN ACCOUNT</div>
+              </Link>
+            </div>
+          </div>
+        </div>
       </section>
       <Footer />
     </>
---
+++ src/CardsJs/Registration.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./CardsHtml/Header";
+import Footer from "./CardsHtml/Footer";
+import "../CardsScc/Registration.css";
+
+export default function Registration() {
+  return (
+    <>
+      <div className="ShopHeader">
+        <Header />
+      </div>
+      <section></section>
+      <Footer />
+    </>
+  );
+}
